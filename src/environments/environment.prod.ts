4312962f734724d6f38768544c37a948a87c7e61
demonpratapdemon <pratapkumarchandra.96@gmail.com>
2021-04-19 23:38:55 +0530

    Adding Configuration for prod deplyment with proxy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   api_config: {
-    base_url: '',
+    base_url: 'http://1.2.3.4:8090/',
     states_api: '/api/region/in/all/',
     cities_api: '/api/city/in/search/',
     external_api_key: 'b6a17e138fe85188d9ce5c7c457bc56e',
