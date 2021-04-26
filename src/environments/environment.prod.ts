9599d61b12848eef0231afa7d7cfde08662f6bea
demonpratapdemon <pratapkumarchandra.96@gmail.com>
2021-04-26 11:37:49 +0530

    Updating the environment files for deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   api_config: {
-    base_url: 'http://1.2.3.4:8090/',
+    base_url: 'http://192.168.49.2/api/',
     states_api: '/api/region/in/all/',
     cities_api: '/api/city/in/search/',
     external_api_key: 'b6a17e138fe85188d9ce5c7c457bc56e',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,12 @@
 export const environment = {
   production: false,
   api_config: {
-    base_url: 'http://localhost:8080/',
-    states_api: '../assets/States&Cities/states.json',
-    cities_api: '../assets/States&Cities/cities.json',
-    external_api_key: '',
+    base_url: 'http://192.168.49.2/api/',
+    // states_api: '../assets/States&Cities/states.json',
+    // cities_api: '../assets/States&Cities/cities.json',
+    states_api: '/api/region/in/all/',
+    cities_api: '/api/city/in/search/',
+    external_api_key: 'b6a17e138fe85188d9ce5c7c457bc56e',
     external_hospital_api : 'https://api.rootnet.in/covid19-in/hospitals/medical-colleges'
   },
 };
